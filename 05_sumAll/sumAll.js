9a9fc666d8af88e6fe59679d24f81908b5bf8e6d
Aravind <aravindramsharma@gmail.com>
2023-01-25 12:15:02 +0530

    Fifth exercise-sum all  in full working condition is uploaded here

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum;
+  if (a <= 0 || b <= 0) {
+    sum = "ERROR";
+  } else if (typeof a != "number" || typeof b != "number") {
+    sum = "ERROR";
+  } else {
+    sum = 0;
+    if (a < b) {
+      for (let i = a; i <= b; i++) {
+        sum += i;
+      }
+    } else {
+      for (let i = a; i >= b; i--) {
+        sum += i;
+      }
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line

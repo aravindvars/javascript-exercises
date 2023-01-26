d6ae6389ce99ec314cb5f37e0e7ed5f8462b99af
Aravind <aravindramsharma@gmail.com>
2023-01-26 09:04:51 +0530

    Seventh exercise - temp conversion is uploaded. Need to finish the fourth exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function (farenheit) {
+  let celcius;
+  let celcius_rounded;
+  celcius = (5 / 9) * (farenheit - 32);
+  celcius_rounded = Math.round(celcius * 10) / 10;
+  return celcius_rounded;
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+  let farenheit_rounded;
+  farenheit = (9 * celcius) / 5 + 32;
+  farenheit_rounded = Math.round(farenheit * 10) / 10;
+  return farenheit_rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };

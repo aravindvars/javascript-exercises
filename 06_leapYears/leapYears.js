b10f76593424323313b150accda804ed1676148e
Aravind <aravindramsharma@gmail.com>
2023-01-26 08:41:04 +0530

    Sixth exercise - leap years is uploaded

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // let year;
+  if (year % 100 != 0) {
+    if (year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (year % 400 == 0) {
+      return true;
+    } else return false;
+  }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// if year / 4 == 0, then true
+// if year/100 && year/400 == 0, then true

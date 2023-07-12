dc19098570650b82570a66cf5b8563440ec1388e
Aravind <aravindramsharma@gmail.com>
2023-07-12 14:32:13 +0530

    Calculator & Fibonocci Series execises updated

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let addition = a + b;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  if (a > b) {
+    return a - b;
+  } else {
+    return 1;
+  }
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach((num) => (sum += num));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = 1;
+  array.forEach((num) => (product *= num));
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    for (i = n; i > 1; i--) {
+      fact = n * factorial(n - 1);
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let n = 0;
+  let a = 1;
+  let fib;
+  if (+num > 0) {
+    if (+num == 1) {
+      fib = 1;
+    }
+    for (i = 0; i < +num - 1; i++) {
+      fib = n + a;
+      n = a;
+      a = fib;
+    }
+    return fib;
+  } else {
+    return 'OOPS';
+  }
 };
 
 // Do not edit below this line

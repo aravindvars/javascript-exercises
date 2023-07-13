6138c26236b0d5dfefa93855bea07c96d5968db0
Aravind <aravindramsharma@gmail.com>
2023-07-13 12:08:02 +0530

    Get titles execise using array.map function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  return arr.map((book) => book.title);
 };
+// const getTheAuthor = function (arr) {
+//   return arr.map((book) => book.author);
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+// module.exports = getTheAuthor;

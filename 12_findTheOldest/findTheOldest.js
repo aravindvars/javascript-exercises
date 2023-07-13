c56050e238d2468689a74f61a1f1cdce4137b75e
Aravind <aravindramsharma@gmail.com>
2023-07-13 14:13:01 +0530

    Array map and sort methods used to find the oldest in an array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const date = new Date();
+  const year = array
+    .map((yea) => {
+      if (!yea.yearOfDeath) {
+        yea.yearOfDeath = date.getFullYear();
+      }
+      return yea;
+    })
+    .sort((a, b) =>
+      a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1
+    );
 
+  // const maxAge = year.sort(function (a, b) {
+  //   return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth
+  //     ? 1
+  //     : -1;
+  // });
+  return (oldest = year[array.length - 1]);
+
+  // const maxiAge = array
+  //   .reduce(function (name) {
+  //     const date = new Date();
+  //     if (!name.yearOfDeath) {
+  //       name.yearOfDeath = date.getFullYear();
+  //     }
+  //   }, 0)
+  //   .sort((a, b) =>
+  //     a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1
+  //   );
+  // return (oldest = maxAge[array.length - 1]);
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;

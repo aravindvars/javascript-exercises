d6a996d44b20baf2bb8c1d2ec341a7f7693ac3c4
Aravind <aravindramsharma@gmail.com>
2023-07-14 23:14:37 +0530

    Palindrome execise with STring to array and replace string elements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let string2 = string.toLocaleLowerCase().replace(/[!,.,,, ]/g, '');
+  let array = string2.split('');
+  let name = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    name.push(array[i]);
+  }
+  let text_rev = name.map((n) => n).join('');
+  if (text_rev === string2) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
